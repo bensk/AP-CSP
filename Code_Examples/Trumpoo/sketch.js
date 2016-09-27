6a4568a5d067cbaee9c0f7bcc3a8537a23598626
Mr. S-K <bensamuelskalow@gmail.com>
2016-09-26 22:02:00 -0400

    Trump 💩

---
+++ Code_Examples/Trumpoo/sketch.js
@@ -0,0 +1,48 @@
+var img;
+var x;
+var y;
+var xDirection = 1;
+var yDirection = 1;
+var speed = 5;
+
+function preload() {
+	img = loadImage("images/donald-dump-dribbble.png")
+}
+
+function setup() {
+
+	createCanvas(windowWidth, windowHeight)
+
+	x = random(0, width)
+	y = random(0, height)
+}
+
+function draw() {
+
+	background('white')
+
+	strokeWeight(20 - y / 20)
+
+	// line(windowWidth / 2, 0, x, y)
+	strokeWeight(10)
+	rectMode(CENTER)
+
+	if (keyIsPressed) {
+		stroke('#D4171C')
+		rect(x, y, 25, 25)
+		noStroke()
+		textSize(18)
+		text("You pressed a key!", 20, 20)
+	} else {
+		// ellipse(x, y, 25, 25)
+		image(img, x, y, 50, 50);
+	}
+
+	x = x + xDirection * speed
+	y = y + yDirection * speed
+	if (x >= width - 50 || x <= 0) {
+		xDirection = xDirection * -1;
+	} else if (y >= height - 50 || y <= 0) {
+		yDirection = yDirection * -1;
+	}
+}
