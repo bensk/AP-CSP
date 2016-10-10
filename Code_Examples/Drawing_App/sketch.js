ee26b9d8b55a36eb6ddd027e39c1ca76a0525c11
Mr. S-K <bensamuelskalow@gmail.com>
2016-10-10 17:23:01 -0400

    🖌Drawing App!

---
+++ Code_Examples/Drawing_App/sketch.js
@@ -0,0 +1,45 @@
+var strokeColor = 'black';
+weight = 5;
+
+
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+
+	if (mouseIsPressed) {
+		strokeWeight(weight)
+		stroke(strokeColor);
+		// strokeWeight(abs(mouseY-pmouseY)/(mouseX-pmouseX))
+
+		line(pmouseX, pmouseY, mouseX, mouseY);
+	}
+}
+
+function windowResized() {
+	resizeCanvas(windowWidth, windowHeight);
+}
+
+
+
+function keyTyped() {
+	if (key === 'b') {
+		strokeColor = 'black';
+		weight = 5;
+	} else if (key === 'p') {
+		strokeColor = '#E32173';
+		weight = 5;
+	} else if (key === 'r') {
+		strokeColor = '#890424';
+		weight = 5;
+	} else if (key === 'u') {
+		strokeColor = '#7E2BE1';
+		weight = 5;
+	} else if (key === 'e') {
+
+		strokeColor = 'white';
+		weight = 50;
+	}
+}
