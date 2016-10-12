5a82a5dfed63b9234fb173ae2a5b3837002e54fb
Mr. S-K <bensamuelskalow@gmail.com>
2016-10-12 17:14:30 -0400

    0⃣0⃣

--- Code_Examples/007/sketch.js
+++ Code_Examples/007/sketch.js
@@ -5,28 +5,29 @@ var fillColor = 'black'
 var speed = 10;
 
 function setup() {
-	createCanvas(windowWidth, windowHeight)
+    createCanvas(windowWidth, windowHeight)
 }
 
 function draw() {
-	
-	noStroke()
-	background(bgColor)
-	text("Speed = " + speed, 10, 10)
-	fill(fillColor)
-	// fill(0,abs(x-256) , abs(x-256))
-	ellipse(windowWidth/2, windowHeight/2, x, x)
-	for (i = 0; i <= speed; i++) {
-		x = x + 1 * direction
-		if (x > windowWidth || x < 0) {
-			direction = direction * -1
-		}
-		if(direction != 1){
-			bgColor = 'black'
-			fillColor = 'white'
-		} else{
-			bgColor = 'white'
-			fillColor = 'black'
-		}
-	}
+
+    noStroke()
+    background(bgColor)
+        // text("Speed = " + speed, 10, 10)
+    fill(fillColor)
+        // fill(0,abs(x-256) , abs(x-256))
+    ellipse(windowWidth / 2, windowHeight / 2, x, x)
+
+    for (i = 0; i <= speed; i++) {
+        x = x + 1 * direction
+        if (x > windowWidth || x < 0) {
+            direction = direction * -1
+        }
+        if (direction != 1) { // if getting smaller...
+            bgColor = 'black' // black background
+            fillColor = 'white' // white ellipse
+        } else {
+            bgColor = 'white' // white background
+            fillColor = 'black' // black ellipse
+        }
+    }
 }
