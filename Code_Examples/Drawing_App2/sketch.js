f1cba2c3ada6c9b2ce1d634ac12116957781dd18
Mr. S-K <bensamuelskalow@gmail.com>
2016-10-16 17:21:28 -0400

    🖌v2.0

--- Code_Examples/Drawing_App2/sketch.js
+++ Code_Examples/Drawing_App2/sketch.js
@@ -2,31 +2,47 @@ var fillColor = 'black'
 var weight = 5;
 
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	background('white')
-	blue_button = createButton('Blue');
-	blue_button.position(10, 10);
-	blue_button.mousePressed(blueColor);
-	black_button = createButton('Black');
-	black_button.position(50,10);
-	black_button.mousePressed(blackColor);
+    createCanvas(windowWidth, windowHeight);
+    background('white')
+    blue_button = createButton('Blue');
+    blue_button.position(10, 10);
+    blue_button.mousePressed(blueColor);
+    black_button = createButton('Black');
+    black_button.position(60, 10);
+    black_button.mousePressed(blackColor);
+    eraser = createButton('Eraser');
+    eraser.position(115, 10);
+    eraser.mousePressed(erase);
 }
 
 function draw() {
-	stroke(fillColor)
-	strokeWeight(weight)
-	if (mouseIsPressed) {
-		line(pmouseX, pmouseY, mouseX, mouseY)
-	}
+    console.log(fillColor)
+    stroke(fillColor)
+    strokeWeight(weight)
+    if (mouseIsPressed) {
+        line(pmouseX, pmouseY, mouseX, mouseY)
+    }
 
 
 }
 
 function blueColor() {
-	fillColor = 'blue'
+    fillColor = 'blue'
+	weight = 5;
 }
 
 function blackColor() {
-	fillColor = 'black'
-	weight = 10
+    fillColor = 'black';
+    weight = 5;
+    // weight = 10
+}
+
+function erase() {
+    fillColor = 'white'
+    weight = 50
+	
+}
+
+function changeColor(r, g, b) {
+    fillColor = r, g, b
 }
