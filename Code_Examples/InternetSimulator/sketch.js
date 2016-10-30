ecb08b8065c9a2dd5243ef10d45d4af817ae3380
Mr. S-K <bensamuelskalow@gmail.com>
2016-10-30 11:36:48 -0400

    :sa: stae

--- Code_Examples/InternetSimulator/sketch.js
+++ Code_Examples/InternetSimulator/sketch.js
@@ -1,5 +1,6 @@
 var signal = 'red'
 var message = ""
+var state
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
@@ -8,7 +9,7 @@ function setup() {
 function draw() {
 	background('white')
 	textSize(14)
-	// text(mouseX + " | " + mouseY, 40, 12);
+		// text(mouseX + " | " + mouseY, 40, 12);
 	stroke(signal)
 	strokeWeight(10)
 	noCursor()
@@ -16,20 +17,26 @@ function draw() {
 	strokeWeight(1)
 	stroke('black')
 	ellipse(mouseX, mouseY, 50, 50);
-	
+
 	textAlign('center')
 	text("Node 1", mouseX - 17, mouseY - 15, 40, 40);
 	ellipse(windowWidth / 2, windowHeight / 2, 50, 50);
-	text("Node 2", windowWidth / 2 -17, windowHeight / 2 - 15, 40, 40);
+	text("Node 2", windowWidth / 2 - 17, windowHeight / 2 - 15, 40, 40);
 	// Change the wire color!
 	if (mouseIsPressed) {
 		signal = 'green'
+		state = 0
 
 	} else {
 		signal = 'red'
+		state = 1
 
 	}
-	textSize(32)
-	
-	text(signal, windowWidth/2, 200)
+	textSize(100)
+textFont('monospace')
+	text("State=" +state, windowWidth / 2, 200)
+}
+
+function windowResized(){
+	resizeCanvas(windowWidth,windowHeight)
 }
