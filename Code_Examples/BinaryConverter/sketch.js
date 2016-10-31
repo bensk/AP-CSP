54f6cc44aa7f9e5cc262825d12e60e11b2fa2324
Mr. S-K <bensamuelskalow@gmail.com>
2016-10-30 20:12:38 -0400

    ⬇arrow

--- Code_Examples/BinaryConverter/sketch.js
+++ Code_Examples/BinaryConverter/sketch.js
@@ -1,22 +1,37 @@
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	input = createInput()
-	input.position(10, 10)
+
+	input.position(10, 30)
 }
 
 function draw() {
-	textAlign('right')
-	textSize(24);
-
 	background('white');
+	textAlign('left')
+	
+	textFont('sans-serif')
+	textSize(12)
+	text('⬇Type a number here ⬇',10,20);
+	textSize(24);
+	textAlign('right')
+
+
+	
 	var number = int(input.value());
 	var num = number.toString(2);
+	var num16 = number.toString(16);
 
 	if (num >= 0) {
-		textFont('sans-serif')
-		text('In decimal ' + number + ' is', windowWidth - 10, 50);
+
+
+		text('In binary, ' + number + ' is', windowWidth - 10, 50);
 		textFont('courier');
-		text(num.toString(2), windowWidth - 10, 80);
+		text(num, windowWidth - 10, 80);
+		textSize(12);
+		text("and in hexadecimal (base 16), " + number + " is", windowWidth - 10, 110)
+		textSize(24);
+		text(num16, windowWidth - 10, 140)
+
 	}
 }
 
