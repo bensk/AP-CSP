5791eba77a53ba7b4213d15032920bf96cad3c05
Ben Samuels-Kalow <bensamuelskalow@gmail.com>
2016-12-16 13:42:15 -0500

    iamge

--- Code_Examples/PathAlongEdges/sketch.js
+++ Code_Examples/PathAlongEdges/sketch.js
@@ -6,55 +6,55 @@ var slider;
 
 
 function setup() {
-  createCanvas(windowWidth, windowHeight)
-  x = 30
-  y = 30
-  speed = 3
-  slider = createSlider(-30, 30, 5);
-  slider.position(windowWidth / 2, windowHeight / 2);
-  slider.style('width', '180px');
+    createCanvas(windowWidth, windowHeight)
+    x = 30
+    y = 30
+    speed = 3
+    slider = createSlider(-30, 30, 5);
+    slider.position(windowWidth / 2, windowHeight / 2);
+    slider.style('width', '180px');
 
 }
 
 function draw() {
 
-  var speed = slider.value();
+    var speed = slider.value();
 
-  background('white')
+    background('white')
 
-  noStroke()
+    noStroke()
 
-  textSize(24)
+    textSize(24)
 
-  text("Moving around the edge", windowWidth / 2, windowHeight / 2 - 32)
+    text("Moving around the edge", windowWidth / 2, windowHeight / 2 - 32)
 
-  text("Speed = " + speed + " | x=" + x + " | " + "y = " + y, windowWidth / 2, windowHeight / 2)
-  textSize(32)
-  text("🤖", x, y)
-    // ellipse(x, y, 50, 50)
-  if (y <= 30) {
+    text("Speed = " + speed + " | x=" + x + " | " + "y = " + y, windowWidth / 2, windowHeight / 2)
+    textSize(32)
+    text("🤖", x, y)
+        // ellipse(x, y, 50, 50)
+    if (y <= 30) {
 
-    x = x + 1 * speed
-  }
-  if (y >= windowHeight - 10) {
-    x = x - 1 * speed
-  }
-  if (x >= windowWidth - 40) {
-    y = y + 1 * speed
-  }
-  if (x <= 5) {
-    y = y - 1 * speed
-  }
+        x = x + 1 * speed
+    }
+    if (y >= windowHeight - 10) {
+        x = x - 1 * speed
+    }
+    if (x >= windowWidth - 40) {
+        y = y + 1 * speed
+    }
+    if (x <= 5) {
+        y = y - 1 * speed
+    }
 }
 
 function keyTyped() {
-  if (key == "=") {
-    speed = speed + 5
-  } else if (key == "-") {
-    speed = speed - 5
-  }
+    if (key == "=") {
+        speed = speed + 5
+    } else if (key == "-") {
+        speed = speed - 5
+    }
 }
 
 function windowResized() {
-  resizeCanvas(windowWidth, windowHeight)
+    resizeCanvas(windowWidth, windowHeight)
 }
